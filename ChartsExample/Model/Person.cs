1fe46c6a6cbd7c64d3dacff0204682641f956f70
User <User@LAB2-1>
2022-01-31 12:22:00 +0200

    Add project files.

---
+++ ChartsExample/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartsExample.Model
+{
+    class Person
+    {
+        public float Height { get; set; }
+    }
+}
---
+++ ChartsExample/ViewModels/ChartsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SkiaSharp;
+using Microcharts;
+using ChartsExample.Model;
+
+using System.ComponentModel;
+
+namespace ChartsExample.ViewModels
+{
+    class ChartsViewModel:INotifyPropertyChanged
+    {
+
+    }
+}
